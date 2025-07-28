6df2cf03614f755a0ac536f6ce835a99c8fea38f
MeenuMariaRoy <meenu.ro@gmail.com>
2025-07-28 18:00:20 +0530

    Asian time changes added

---
+++ src/test/java/seleniumCourse/seleniumFrameDesign/GitDemo.java
@@ -0,0 +1,14 @@
+package seleniumCourse.seleniumFrameDesign;
+
+import org.testng.annotations.Test;
+
+public class GitDemo {
+	
+	@Test
+	public void gitDemoTest()
+	{
+		System.out.println("Im a demo for git test in my work folder");
+	}
+
+}
+
