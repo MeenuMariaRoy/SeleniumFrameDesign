1f979f541afde1d305f2be284a7de1dbe650e092
MeenuMariaRoy <meenu.ro@gmail.com>
2025-07-29 11:54:54 +0530

    Architect Asian time

---
+++ src/test/java/seleniumCourse/seleniumFrameDesign/GitBranch.java
@@ -0,0 +1,25 @@
+package seleniumCourse.seleniumFrameDesign;
+
+import org.testng.annotations.Test;
+
+public class GitBranch {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("Branch check 1");
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("Branch check 2");
+	}
+	
+	@Test
+	public void method3()
+	{
+		System.out.println("Branch check 3");
+	}
+
+}
